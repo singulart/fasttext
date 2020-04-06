abb17c5558415ded166cbfb50e8a290665723300
Singulart <serpian@gmail.com>
2020-04-06 18:50:38 +0300

    simple lemmatizing/vectorizing routine

--- normalize.sh
+++ normalize.sh
@@ -11,7 +11,7 @@ normalize_text() {
   tr '[:upper:]' '[:lower:]' | sed -e 's/^/__label__/g' | \
     sed -e "s/'/ ' /g" -e 's/"//g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' \
         -e 's/,/ , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
-        -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' | tr -s " " # | myshuf
+        -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' | tr -s " " | myshuf
 }
 
 cat $1 | normalize_text > 'normie.txt'
