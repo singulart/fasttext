d17e3a035ce3472e2a97af7e423993dda796a063
Singulart <serpian@gmail.com>
2020-04-07 11:58:25 +0300

    word for nearest neighbour lookup is provided via command line arg

--- nn.py
+++ nn.py
@@ -1,7 +1,8 @@
 from nn.fasttextnn import FastTextNN
 import fasttext
+import sys
 
 model = fasttext.load_model('result.bin')
-print('Model loaded...')
+print('Nearest neighbours for %s are ...' % sys.argv[1])
 fasttext_nn = FastTextNN(model)
-print(fasttext_nn.nearest_words('сторона'))
+print(fasttext_nn.nearest_words(sys.argv[1]))
